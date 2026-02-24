ac98d8200d0d7e56c8a1ef873fdb51d6c94727b4
2009Bence <benceviasz@gmail.com>
2026-02-24 16:18:40 +0100

    Delete app directory

--- app/app/calendar/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function CalendarPage() {
-  return (
-    <div>
-      <h1 style={{ marginTop: 0 }}>Naptár</h1>
-      <p>Calendar view placeholder.</p>
-    </div>
-  );
-}
--- app/app/settings/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function SettingsPage() {
-  return (
-    <div>
-      <h1 style={{ marginTop: 0 }}>Beállítások</h1>
-      <p>Settings placeholder.</p>
-    </div>
-  );
-}
