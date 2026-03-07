3f73c769a34b94bc081d85f1159977b1b921749a
2009Bence <benceviasz@gmail.com>
2026-03-08 00:02:46 +0100

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,3 +1,7 @@
 const supabaseUrl = "https://kskvssesdnnivawsfjfm.supabase.co";
 const supabaseKey = "sb_publishable_ARj6K36ENV8g9LOtZrWwXw_tBK00b9g";
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+
+console.log("Supabase config betöltve");
+console.log("URL:", supabaseUrl);
+console.log("Client:", supabase);
