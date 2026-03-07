a226274556a653eafac533f2c116bc692c4725a3
2009Bence <benceviasz@gmail.com>
2026-03-08 00:01:51 +0100

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,2 +1,3 @@
-export const supabaseUrl = "sb_publishable_ARj6K36ENV8g9LOtZrWwXw_tBK00b9g";
-export const supabaseUrl = "https://kskvssesdnnivawsfjfm.supabase.co";
+const supabaseUrl = "https://kskvssesdnnivawsfjfm.supabase.co";
+const supabaseKey = "sb_publishable_ARj6K36ENV8g9LOtZrWwXw_tBK00b9g";
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
